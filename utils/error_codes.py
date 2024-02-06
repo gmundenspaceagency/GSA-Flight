995559aaf358edfdf8e5727a54b924942f7fd359
flerovium11 <enniobinder@gmail.com>
2024-02-06 13:05:34 +0100

    first custom error codes

---
+++ utils/error_codes.py
@@ -0,0 +1,10 @@
+ERROR_CODES = {
+    "01": "Light sensor 1 not responding",
+    "02": "Light sensor 2, 3 or multiplexer not responding",
+    "03": "A/D-Converter not responding",
+    "04": "BME280 not responding",
+    "05": "Gyroscope not responding",
+    "06": "Error in motor script",
+    "07": "Camera not responding",
+    
+}
