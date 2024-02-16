e0831c1ba35fcd8d780208dd47dd018381abaa71
glaze <glaze@LAPTOP-LEO2SOUT>
2024-02-16 23:59:45 +0100

    minor changes

--- tests/gt-u7_test.py
+++ tests/gt-u7_test.py
@@ -2,9 +2,8 @@ import serial
 import time
 import pynmea2
 
-def extract_lat_lon(nmea_string):
+def extract_lat_lon(nmea_string, data_type):
     lines = nmea_string.split('\n')
-
     for line in lines:
         if line.startswith("$GPRMC"):
             try:
@@ -12,9 +11,12 @@ def extract_lat_lon(nmea_string):
                 if msg.lat != '':
                     latitude = msg.lat
                     longitude = msg.lon
-                    lat_decimal = lat_convert_to_decimal(latitude)
-                    lon_decimal = lon_convert_to_decimal(longitude)
-                    print("lat,lon:","{},{}".format(lat_decimal, lon_decimal))
+                    if data_type == 'decimal':
+                        lat_decimal = lat_convert_to_decimal(latitude)
+                        lon_decimal = lon_convert_to_decimal(longitude)
+                        print("lat,lon:","{},{}".format(lat_decimal, lon_decimal))
+                    if data_type == 'dms':
+                        print("lat,lon:","{},{}".format(latitude, longitude))
             except pynmea2.ParseError as e:
                 print('Parse error: {}'.format(e))
 def extract_altitude(nmea_string):
@@ -43,6 +45,22 @@ def extract_velocity(nmea_string):
                     print("velocity:", speed_ms)
             except pynmea2.ParseError as e:
                 print('Parse error: {}'.format(e))
+
+def extract_satellite_count(nmea_string):
+    lines = nmea_string.split('\n')
+
+    for line in lines:
+        if line.startswith("$GPGGA"):
+            try:
+                msg = pynmea2.parse(line)
+                if msg.num_sats != '':
+                    num_satellites = int(msg.num_sats)
+                    if num_satellites > 0:
+                        print("satelite count: {}".format(num_satellites))
+                    else:
+                        print("no satelites found!")
+            except pynmea2.ParseError as e:
+                print('Parse error: {}'.format(e))
                 
 def lat_convert_to_decimal(coord_str):
     degrees = int(coord_str[:2])
@@ -63,7 +81,8 @@ serialPort = serial.Serial(port, baudrate=baud, timeout=1)
 while True:
     nmea_sentence = serialPort.readline().decode().strip()
     #print(nmea_sentence)
-    extract_lat_lon(nmea_sentence)
+    extract_lat_lon(nmea_sentence, "decimal")
     extract_altitude(nmea_sentence)
     extract_velocity(nmea_sentence)
+    extract_satellite_count(nmea_sentence)
 
