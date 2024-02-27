af9c596a87564c3c2e921f135faccdf6020ba785
flerovium11 <enniobinder@gmail.com>
2024-02-27 23:03:23 +0100

    add multiplexer class and beeper

--- flight.py
+++ flight.py
@@ -13,8 +13,9 @@ from time import sleep, perf_counter
 # pimoroni-bme280 1.0.0
 from bme280 import BME280
 from Adafruit_ADS1x15 import ADS1115
-from gsa_components.mpu6050.mpu6050 import Mpu6050
-from gsa_components.bh1750.bh1750 import Bh1750
+from gsa_components.mpu6050 import Mpu6050
+from gsa_components.multiplexer import Multiplexer 
+from gsa_components.bh1750 import Bh1750
 from gsa_components.motor import StepperMotor
 from gsa_components.rak4200 import Rak4200
 from tests import CircularPIDController
@@ -41,33 +42,34 @@ Allgemeine ToDos:
 
 CANSAT_ID = "69xd"
 MODE = "groundtest" # either "groundtest" or "flight"
-BUS = smbus.SMBus(1)
-channel_array=[0b00000001,0b00000010,0b00000100,0b00001000,0b00010000,0b00100000,0b01000000,0b10000000]
 pi_state = "initializing"
 print("Pi is initializing...")
 
-# is True when a second thread with the blink_onboard function is running
+# is True when a second thread with the blink_status function is running
 blinking = False
 
-# initialize and test onboard led
+# initialize and test status led
 try:
     status_led = LED(25)
 
-    # controls blinking of pi led to indicate different states of pi
-    def blink_onboard(timeout: float, state: str) -> None:
+    # controls blinking of status led to indicate different states of pi
+    def blink_status(seq: list[float], state: str) -> None:
         global blinking
         blinking = True
 
         while pi_state == state:
-            status_led.on()
-            sleep(timeout)
-            status_led.off()
-            sleep(timeout)
+            for timeout in seq:
+                if status_led.is_lit:
+                    status_led.off()
+                else:
+                    status_led.on()
+                
+                sleep(timeout)
 
         blinking = False
 
     # blinking to show pi is initializing
-    Thread(target=blink_onboard, args=(0.5, "initializing")).start()
+    Thread(target=blink_status, args=([0.5], "initializing")).start()
 except Exception as error:
     print("Warning: Error while initializing onboard LED: " + str(error))
 
@@ -82,14 +84,22 @@ def initialize_light1()->Optional[Bh1750]:
     
     return light1
 
-def initialize_light2n3()->Optional[Bh1750]:   
+def set_multiplexer_channel(channel:int)->None:
     try:
-        BUS.write_byte(0x70, channel_array[6])
+        multiplexer.switch_to(channel)
+    except Exception as error:
+        # try to contact module again
+        multiplexer = initialize_multiplexer()
+
+def initialize_light2n3()->Optional[Bh1750]:
+    set_multiplexer_channel(6)
+   
+    try:
         light2n3 = Bh1750()
         light2n3.luminance(Bh1750.ONCE_LOWRES)
     except Exception as error:
         light2n3 = None
-        print("Problem with light sensor 2+3 or Multiplexer: " + str(error))
+        print("Problem with light sensor 2 or 3: " + str(error))
     
     return light2n3
 
@@ -159,14 +169,29 @@ def initialize_camera()->Optional[picamera.PiCamera]:
     
     return camera
 
+def initialize_multiplexer()->Optional[Multiplexer]:
+    try:
+        multiplexer = Multiplexer()
+    except Exception as error:
+        multiplexer = None
+        print("Problem with multiplexer: " + str(error))
+    
+    return multiplexer
+
 try:
     GPIO.setmode(GPIO.BCM)
 
     # not using Button class of gpiozero because of weird error when starting from crontab
     power_button = 10
     GPIO.setup(power_button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    beeper = LED(21)
+    beeper.on()
+    sleep(0.5)
+    beeper.off()
     
     # every sensor is in a try-block so the program will still work when one of the sensors fails on launch
+    multiplexer = initialize_multiplexer()
     light1 = initialize_light1()
     light2n3 = initialize_light2n3()
     ads1115 = initialize_ads1115()
@@ -174,7 +199,7 @@ try:
     mpu6050 = initialize_mpu6050()
     motor = initialize_motor()
     guenther = initialize_guenther()
-    camera = initialize_camera()    
+    camera = initialize_camera() 
 except KeyboardInterrupt:
     print("Initializing aborted by keyboard interrupt")
     GPIO.cleanup()
@@ -184,12 +209,12 @@ try:
     pi_state = "ready"
     print("Pi is ready, hold start button to start program")
 
-    # wait until the second thread with the blink_onboard function has stopped
+    # wait until the second thread with the blink_status function has stopped
     while blinking:
         pass
 
     # blinking to show the pi is ready
-    Thread(target=blink_onboard, args=(0.1, "ready")).start()
+    Thread(target=blink_status, args=([0.1], "ready")).start()
 
     # wait for 1s button press
     while True:
@@ -200,7 +225,7 @@ try:
                 pass
 
             # fast flashing telling you should keep holding the button
-            Thread(target=blink_onboard, args=(0.05, "starting")).start()
+            Thread(target=blink_status, args=([0.05], "starting")).start()
             sleep(1)
 
             if GPIO.input(power_button) == GPIO.LOW:
@@ -211,7 +236,7 @@ try:
                 while blinking:
                     pass
 
-                Thread(target=blink_onboard, args=(0.5, "ready")).start()
+                Thread(target=blink_status, args=([0.5], "ready")).start()
 except KeyboardInterrupt:
     print("Program stopped in ready state by keyboard interrupt")
     GPIO.cleanup()
@@ -220,7 +245,7 @@ except KeyboardInterrupt:
 luminance1 = luminance2 = luminance3 = None
 
 def rotation_mechanism() -> None:
-    global luminance1, luminance2, luminance3
+    global light1, light2n3, multiplexer, luminance1, luminance2, luminance3
     
     while pi_state == "descending":            
         try:  
@@ -230,9 +255,9 @@ def rotation_mechanism() -> None:
                 # try to contact sensor again
                 light1 = initialize_light1()
             try:
-                BUS.write_byte(0x70, channel_array[7])
+                set_multiplexer_channel(7)
                 luminance2 = round(light2n3.luminance(Bh1750.ONCE_LOWRES), 4)
-                BUS.write_byte(0x70, channel_array[6])
+                set_multiplexer_channel(6)
                 luminance3 = round(light2n3.luminance(Bh1750.ONCE_LOWRES), 4)
             except Exception as error:
                 # try to contact sensor again
@@ -261,7 +286,7 @@ def rotation_mechanism() -> None:
             print("Error in rotation mechanism: " + str(error))
 
 def main()->None:
-    global pi_state, ads1115, bme280, mpu6050, guenther, camera
+    global pi_state, ads1115, bme280, mpu6050, guenther, camera, multiplexer
 
     start_time = datetime.datetime.now()
     start_time_str = start_time.strftime("%Y-%m-%d_%H-%M-%S")
@@ -517,8 +542,13 @@ def main()->None:
         status_led.off()
         timestamp = round(perf_counter() * 1000 - start_perf)
         timestamps.append(timestamp)
+        
+        if beeper.is_beeping:
+            beeper.off()
+        else:
+            beeper.on()
 
-        #TODO: add a landed mode with coordinates, beeping and stuff
+        #TODO: add a landed mode with coordinates
 
         with open(logfile_path, "a") as logfile:
             logfile.write(f"{timestamp},,,,,,,,,,,,,,,,,,,None,{pi_state}\n")
@@ -539,7 +569,7 @@ def main()->None:
                 pass
             
             # fast flashing telling pi is about to shut down
-            Thread(target=blink_onboard, args=(0.05, "shutting down")).start()
+            Thread(target=blink_status, args=([0.05], "shutting down")).start()
             sleep(1)
 
             if GPIO.input(power_button) == GPIO.LOW:
--- utils/info_codes.py
+++ utils/info_codes.py
@@ -2,12 +2,13 @@
 These are pre-written error and info codes intended to be sent to the ground station
 Keys are one-digit HEX-Numbers to minimize data sent to ground
 Specific errors might be too long to transmit and therefore will be saved in a separate file locally
+There is no code for 'Ground mode' because we can just leave the mode info empty to inicate pi is in ground mode
 """
 
 INFO_CODES = {
     "0": "Error in camera recording",
     "1": "Light sensor 1 not responding",
-    "2": "Light sensor 2, 3 or multiplexer not responding",
+    "2": "Light sensor 2 or 3 not responding",
     "3": "A/D-Converter not responding",
     "4": "BME280 not responding",
     "5": "Gyroscope not responding",
@@ -16,7 +17,7 @@ INFO_CODES = {
     "8": "GPS Module not responding",
     "9": "Unknown error in rotation thread",
     "A": "Unknown error in main loop",
-    "B": "Ground mode",
+    "B": "Multiplexer not responding",
     "C": "Ascending",
     "D": "Descending",
     "E": "Landed",
