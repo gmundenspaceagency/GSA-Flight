61d2ec1a7e599f88e872a88394deafd371eabf5f
gsa202324 <gsa202324@GSA-Bodenstation.myguest.virtualbox.org>
2024-03-09 00:31:38 +0100

    add parameter setting

--- tests/rak4200_test.py
+++ tests/rak4200_test.py
@@ -16,7 +16,7 @@ Parameters:
     - preamble_length (int): Preamble length. Range: 5 to 65535. Default is 8.
     - tx_power (int): Transmit power in dBm. Range: 5 to 20. Default is 20.
 """
-parameters = (869.525, 7, 1, 1, 8, 20)
+parameters = (869.525, 12, 0, 1, 8, 20)
 
 send_timeout = 0.5 # only for sending
 
@@ -38,17 +38,22 @@ if mode == 'receive':
                 if received == 0:
                     start = perf_counter()
                 
-                last_index = int(data['message'].split(';')[0])
-                strenghts.append(data['signal_strength'])
-                noises.append(data['noise'])
-                received += 1
-                print(f'Received: {data} (lost: {last_index - received}, total: {last_index})')
+                if not '(Info)' in data['message']:
+                    last_index = int(data['message'].split(';')[0])
+                    strenghts.append(data['signal_strength'])
+                    noises.append(data['noise'])
+                    received += 1
+                    print(f'Received: {data} (lost: {last_index - received}, total: {last_index})')
+                else:
+                    print(data['message'])
+                
+
     except KeyboardInterrupt:
         end = perf_counter()
         average_strength = round(sum(strenghts) / received, 2)
         print(noises)
         average_noise = round(sum(noises) / received, 2)
-        print('------------RESULT------------')
+        print('\n\n------------RESULT------------')
         print(f'LISTENED FOR: {round(end - start, 2)}s')
         print(f'RECEIVED: {received}')
         print(f'LOST: {last_index - received}')
