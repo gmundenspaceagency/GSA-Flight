60d93c0cd6c944bd49b6ab9d0cf982aa9a174280
Robert <glazers.robert@gmail.com>
2024-03-22 16:11:22 +0100

    test transfer to pi

--- gsa_components/gt_u7/gt_u7.py
+++ gsa_components/gt_u7/gt_u7.py
@@ -3,16 +3,19 @@ import time
 
 class Gt_u7:
     def get_lat(self, nx):
+        nx = gpsd.next()
         if nx['class'] == 'TPV':
             latitude = getattr(nx, 'lat', "Unknown")
             return str(latitude)
 
     def get_lon(self, nx):
+        nx = gpsd.next()
         if nx['class'] == 'TPV':
             longitude = getattr(nx, 'lon', "Unknown")
             return str(longitude)
         
     def get_altitude(self, nx):
+        nx = gpsd.next()
         if nx['class'] == 'TPV':
             altitude = getattr(nx, 'alt', "Unknown")
             return str(altitude)
@@ -21,8 +24,7 @@ if __name__ == "__main__":
     gt_u7 = Gt_u7()
     gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE)
     while True:
-        nx = gpsd.next()
-        print(gt_u7.get_lat(nx))
-        print(gt_u7.get_lon(nx))
-        print(gt_u7.get_altitude(nx))
+        print(gt_u7.get_lat(gpsd))
+        print(gt_u7.get_lon(gpsd))
+        print(gt_u7.get_altitude(gpsd))
         time.sleep(1)
