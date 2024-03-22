06db71e86261bec46af2951673e768661b980abf
Robert <glazers.robert@gmail.com>
2024-03-22 19:39:59 +0100

    transfer_to_pi_commit

--- gsa_components/gt_u7/gt_u7.py
+++ gsa_components/gt_u7/gt_u7.py
@@ -1,30 +1,32 @@
-from gps import *
 import time
+from gps import *
 
 class Gt_u7:
-    def get_lat(self, nx):
-        nx = gpsd.next()
+    def __init__(self):
+        self.gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE)
+
+    def get_lat(self):
+        nx = self.gpsd.next()
         if nx['class'] == 'TPV':
             latitude = getattr(nx, 'lat', "Unknown")
             return str(latitude)
 
-    def get_lon(self, nx):
-        nx = gpsd.next()
+    def get_lon(self):
+        nx = self.gpsd.next()
         if nx['class'] == 'TPV':
             longitude = getattr(nx, 'lon', "Unknown")
             return str(longitude)
         
-    def get_altitude(self, nx):
-        nx = gpsd.next()
+    def get_altitude(self):
+        nx = self.gpsd.next()
         if nx['class'] == 'TPV':
             altitude = getattr(nx, 'alt', "Unknown")
             return str(altitude)
 
 if __name__ == "__main__":
     gt_u7 = Gt_u7()
-    gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE)
     while True:
-        print(gt_u7.get_lat(gpsd))
-        print(gt_u7.get_lon(gpsd))
-        print(gt_u7.get_altitude(gpsd))
+        print(gt_u7.get_lat())
+        print(gt_u7.get_lon())
+        print(gt_u7.get_altitude())
         time.sleep(1)
