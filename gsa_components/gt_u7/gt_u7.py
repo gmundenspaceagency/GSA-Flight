1bbb4de07e663b25b21ec5b7c3a7c62cae831220
Robert <glazers.robert@gmail.com>
2024-03-22 19:46:18 +0100

    transfer_to_pi_commit

--- gsa_components/gt_u7/gt_u7.py
+++ gsa_components/gt_u7/gt_u7.py
@@ -1,32 +1,24 @@
 import time
+import json
 from gps import *
 
 class Gt_u7:
     def __init__(self):
         self.gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE)
 
-    def get_lat(self):
+    def get_data(self):
         nx = self.gpsd.next()
         if nx['class'] == 'TPV':
-            latitude = getattr(nx, 'lat', "Unknown")
-            return str(latitude)
-
-    def get_lon(self):
-        nx = self.gpsd.next()
-        if nx['class'] == 'TPV':
-            longitude = getattr(nx, 'lon', "Unknown")
-            return str(longitude)
-        
-    def get_altitude(self):
-        nx = self.gpsd.next()
-        if nx['class'] == 'TPV':
-            altitude = getattr(nx, 'alt', "Unknown")
-            return str(altitude)
+            latitude = nx.get('lat', "Unknown")
+            longitude = nx.get('lon', "Unknown")
+            altitude = nx.get('alt', "Unknown")
+            return str(latitude), str(longitude), str(altitude)
 
 if __name__ == "__main__":
     gt_u7 = Gt_u7()
     while True:
-        print(gt_u7.get_lat())
-        print(gt_u7.get_lon())
-        print(gt_u7.get_altitude())
+        lat, lon, alt = gt_u7.get_data()
+        print(lat)
+        print(lon)
+        print(alt)
         time.sleep(1)
