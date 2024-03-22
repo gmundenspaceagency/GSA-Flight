01ee22b41fc7cc51d84d7875360046dc89d70251
SimeonNew <simeon.nieuwenhuys7@gmail.com>
2024-03-22 15:44:50 +0100

    transfer_to_pi

--- gsa_components/gt_u7/gt_u7.py
+++ gsa_components/gt_u7/gt_u7.py
@@ -13,7 +13,7 @@ class Gt_u7:
         nx = gpsd.next()
         if nx['class'] == 'TPV':
             longitude = getattr(nx,'lon', "Unknown")
-            return str(longitude)
+            return longitude
         
     def getPositionData(self, gpsd):
         nx = gpsd.next()
@@ -22,19 +22,8 @@ class Gt_u7:
             longitude = getattr(nx,'lon', "Unknown")
             print ("Your position: lon = " + str(longitude) + ", lat = " + str(latitude))
     
-    gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE)
-
-    while True:
-        print(get_lon)
-        getPositionData(gpsd)
-        time.sleep(1.0)
-
-if __name__ == "__main__":
-    gt_u7 = Gt_u7()
-
-    while True:
-        gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE)
-        print("lat:", gt_u7.get_lat(gpsd))
-        print("lon", gt_u7.get_lon(gpsd))
-        gt_u7.getPositionData(gpsd)
-        time.sleep(1.0)
+gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE)
+gt_u7 = Gt_u7()
+while True:
+    print(gt_u7.get_lon(gpsd))
+    time.sleep(1.0)
