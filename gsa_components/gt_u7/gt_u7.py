ff4096297877a709297e8e475db51fd2c056dea4
Robert <glazers.robert@gmail.com>
2024-03-22 15:28:36 +0100

    test transfer to pi

--- gsa_components/gt_u7/gt_u7.py
+++ gsa_components/gt_u7/gt_u7.py
@@ -22,83 +22,12 @@ class Gt_u7:
             longitude = getattr(nx,'lon', "Unknown")
             print ("Your position: lon = " + str(longitude) + ", lat = " + str(latitude))
     
-    def extract_lat(self):
-        nmea_sentence = gt_u7.serialPort.readline().decode().strip()
-        msg = pynmea2.parse(nmea_sentence)
-        return msg.lat
+    gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE)
 
-    def extract_lat_lon(self, nmea_string, data_type):
-        lines = nmea_string.split('\n')
-        for line in lines:
-            if line.startswith("$GPRMC"):
-                try:
-                    msg = pynmea2.parse(line)
-                    if msg.lat != '':
-                        latitude = msg.lat
-                        longitude = msg.lon
-                        if data_type == 'decimal':
-                            lat_decimal = self.lat_convert_to_decimal(latitude)
-                            lon_decimal = self.lon_convert_to_decimal(longitude)
-                            print(lat_decimal, lon_decimal, sep=",")
-                        if data_type == 'dms':
-                            print(latitude, longitude, sep=",")
-                except pynmea2.ParseError as e:
-                    print('Parse error: {}'.format(e))
-
-    def extract_altitude(self, nmea_string):
-        lines = nmea_string.split('\n')
-
-        for line in lines:
-            if line.startswith("$GPGGA"):
-                try:
-                    msg = pynmea2.parse(line)
-                    if msg.altitude != '':
-                        altitude = msg.altitude
-                        print("altitude:", altitude)
-                except pynmea2.ParseError as e:
-                    print('Parse error: {}'.format(e))
-                    
-    def extract_velocity(self, nmea_string):
-        lines = nmea_string.split('\n')
-        for line in lines:
-            if line.startswith("$GPRMC"):
-                try:
-                    msg = pynmea2.parse(line)
-                    if msg.spd_over_grnd != '':
-                        speed_knots = float(msg.spd_over_grnd)
-                        speed_ms = speed_knots * 0.514444  # 1 Knoten = 0,514444 m/s
-                        print("velocity:", speed_ms)
-                except pynmea2.ParseError as e:
-                    print('Parse error: {}'.format(e))
-
-    def extract_satellite_count(self, nmea_string):
-        lines = nmea_string.split('\n')
-        for line in lines:
-            if line.startswith("$GPGGA"):
-                try:
-                    msg = pynmea2.parse(line)
-                    if msg.num_sats != '':
-                        num_satellites = int(msg.num_sats)
-                        if num_satellites > 0:
-                            print("satellite count:", num_satellites)
-                        else:
-                            print("No satellites found!")
-                    else:
-                        print("No satellite count data available")
-                except pynmea2.ParseError as e:
-                    print('Parse error: {}'.format(e))
-
-    def lat_convert_to_decimal(self, coord_str):
-        degrees = int(coord_str[:2])
-        minutes = float(coord_str[2:])
-        decimal_degrees = round(degrees + (minutes / 60), 8)
-        return decimal_degrees
-
-    def lon_convert_to_decimal(self, coord_str):
-        degrees = int(coord_str[:3])
-        minutes = float(coord_str[3:])
-        decimal_degrees = round(degrees + (minutes / 60), 8)
-        return decimal_degrees
+    while True:
+        print(get_lon)
+        getPositionData(gpsd)
+        time.sleep(1.0)
 
 if __name__ == "__main__":
     gt_u7 = Gt_u7()
@@ -107,5 +36,5 @@ if __name__ == "__main__":
         gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE)
         print("lat:", gt_u7.get_lat(gpsd))
         print("lon", gt_u7.get_lon(gpsd))
-        gt_u7.getPositionData
+        gt_u7.getPositionData(gpsd)
         time.sleep(1.0)
