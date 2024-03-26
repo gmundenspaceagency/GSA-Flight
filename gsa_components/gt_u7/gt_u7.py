50c1641c3524cb986b935ad958bd060245089087
Robert <glazers.robert@gmail.com>
2024-03-26 21:13:52 +0100

    fixing gps errors on pizero

--- gsa_components/gt_u7/gt_u7.py
+++ gsa_components/gt_u7/gt_u7.py
@@ -1,12 +1,48 @@
 import serial
 import pynmea2
 
-# Serielle Verbindung herstellen
-ser = serial.Serial('/dev/ttyACM0', 9600)  # Überprüfe die Baudrate entsprechend deiner Anwendung
+class Gt_u7:
+
+    def __init__(self, serial_port="/dev/ttyACM0", baudrate=9600):
+        self.ser = serial.Serial(serial_port, baudrate)
+
+    def read_data(self):
+        data = self.ser.readline().decode().strip()
+        if data.startswith("$GPGGA"):
+            msg = pynmea2.parse(data)
+            return msg
+        
+    def get_coordinates(self):
+        msg = self.read_data()
+        if msg:
+            latitude = msg.latitude
+            longitude = msg.longitude
+            return latitude, longitude
+        else:
+            return None, None
+        
+    def get_altitude(self):
+        msg = self.read_data()
+        if msg:
+            altitude = msg.altitude
+            return altitude
+        else:
+            return None
+
+if __name__ == "__main__":
+    gps = Gt_u7()
 
     while True:
-        # Daten vom Port lesen
-        data = ser.readline().decode().strip()  # Daten decodieren und unnötige Leerzeichen entfernen
-        
-        # Daten ausgeben
-        print("Empfangene Daten:", data)
+        latitude, longitude = gps.get_coordinates()
+        altitude = gps.get_altitude()
+
+        if latitude is not None and longitude is not None:
+            print("Latitude:", latitude)
+            print("Longitude:", longitude)
+        else:
+            print("Keine GPS-Daten verfügbar.")
+
+        if altitude is not None:
+            print("Höhe:", altitude)
+        else:
+            print("Keine Höhendaten verfügbar.")
