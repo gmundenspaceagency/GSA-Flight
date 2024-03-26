9aca6874a81596105c50eb3b1b5ec1ab1f2685ee
Robert <glazers.robert@gmail.com>
2024-03-26 14:51:45 +0100

    fixing errors on pizero

--- gsa_components/gt_u7/gt_u7.py
+++ gsa_components/gt_u7/gt_u7.py
@@ -1,25 +1,17 @@
-## I got this code from https://maker.pro/raspberry-pi/tutorial/how-to-read-gps-data-with-python-on-a-raspberry-pi
+import gps
 
-from gps import *
-import time
+session = gps.gps("localhost", "2947")
+session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 
-running = True
-
-def getPositionData(gps):
-    nx = gpsd.next()
-    if nx['class'] == 'TPV':
-        latitude = getattr(nx,'lat', "Unknown")
-        longitude = getattr(nx,'lon', "Unknown")
-        print ("Your position: lon = " + str(longitude) + ", lat = " + str(latitude))
-
-gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE)
-
-try:
-    print ("Application started!")
-    while running:
-        getPositionData(gpsd)
-        time.sleep(1.0)
-
-except (KeyboardInterrupt):
-    running = False
-    print ("Applications closed!")
+while True:
+    try:
+        report = session.next()
+        if report['class'] == 'TPV':
+            print(report)
+    except KeyError:
+        pass
+    except KeyboardInterrupt:
+        quit()
+    except StopIteration:
+        session = None
+        print("GPSD has terminated")
