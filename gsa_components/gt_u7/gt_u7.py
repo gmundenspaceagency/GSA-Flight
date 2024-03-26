48743b8d5420262b92274a0d0bd694c3d0a1bc7a
Robert <glazers.robert@gmail.com>
2024-03-26 21:49:12 +0100

    fixing gps errors on pizero

--- gsa_components/gt_u7/gt_u7.py
+++ gsa_components/gt_u7/gt_u7.py
@@ -7,10 +7,11 @@ class Gt_u7:
         self.ser = serial.Serial(serial_port, baudrate)
 
     def read_data(self):
-        data = self.ser.readline().decode().strip()
-        if data.startswith("$GPGGA"):
-            msg = pynmea2.parse(data)
-            return msg
+        while True:
+            data = self.ser.readline().decode().strip()
+            if data.startswith("$GPGGA"):
+                msg = pynmea2.parse(data)
+                return msg
 
     def get_coordinates(self, msg):
         if msg:
