7533997b66a51eaa3e49af7b0b2e65f7ae5a4a69
Robert <glazers.robert@gmail.com>
2024-03-30 20:06:09 +0100

    added threading to gps class

--- gsa_components/gt_u7/gt_u7.py
+++ gsa_components/gt_u7/gt_u7.py
@@ -1,34 +1,38 @@
 import serial
 import pynmea2
+from threading import Thread
 
 class Gt_u7:
-
     def __init__(self, serial_port="/dev/ttyACM0", baudrate=9600):
         self.ser = serial.Serial(serial_port, baudrate)
+        self.latitude = None
+        self.longitude = None
+        self.altitude = None
+        self.stop_thread = False
+        self.thread = Thread(target=self.read_data_thread)
+        self.thread.start()
 
     def read_data(self):
         while True:
             data = self.ser.readline().decode().strip()
             if data.startswith("$GPGGA"):
                 msg = pynmea2.parse(data)
-                return msg
+                self.latitude = msg.latitude
+                self.longitude = msg.longitude
+                self.altitude = msg.altitude
+
+    def read_data_thread(self):
+        self.read_data()
 
     def get_coordinates(self):
-        msg = gps.read_data()
-        if msg:
-            latitude = msg.latitude
-            longitude = msg.longitude
-            return latitude, longitude
-        else:
-            return None, None
+        return self.latitude, self.longitude
 
     def get_altitude(self):
-        msg = gps.read_data()
-        if msg:
-            altitude = msg.altitude
-            return altitude
-        else:
-            return None
+        return self.altitude
+
+    def stop(self):
+        self.stop_thread = True
+        self.thread.join()
 
 if __name__ == "__main__":
     gps = Gt_u7()
@@ -40,4 +44,3 @@ if __name__ == "__main__":
         print(f"lon:{longitude}")
         print(f"lat:{latitude}")
         print(f"alt:{altitude}")
-
