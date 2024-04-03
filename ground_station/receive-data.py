b463317f2e788b0387bed2edb1b57f6cf59018cf
flerovium11 <enniobinder@gmail.com>
2024-04-04 00:25:22 +0200

    fix mistakes in receive-data.py (ground station)

--- ground_station/receive-data.py
+++ ground_station/receive-data.py
@@ -2,7 +2,9 @@ from time import sleep
 import os
 import csv
 import datetime
-from ..gsa_components.rak4200 import Rak4200
+import sys
+sys.path.append('..')
+from gsa_components.rak4200 import Rak4200
 
 try:
     rak = Rak4200(serial_port='/dev/ttyS0')
@@ -10,6 +12,8 @@ try:
 except Exception as error:
     rak.sleep()
     raise Exception('Error while starting RAK: ' + str(error))
+
+print("RAK4200 connnected, receiving data...")
 
 start_time = datetime.datetime.now()
 start_time_str = start_time.strftime("%Y-%m-%d_%H-%M-%S")
@@ -22,33 +26,25 @@ with open(logfile_path, "a") as logfile:
     csv_writer = csv.writer(logfile, delimiter = ";")
     csv_writer.writerow(["Timestamp", "Pressure (hPa)", "Temperature (°C)", "Errors", "Mode", "Signal Strength", "Noise", "Message Length"])
 
-responses = [{
-        'signal_strength': 10,
-        'noise': 20,
-        'message_length': 100,
-        'message': '69xd;10029;press;temp;errr;mod',
-    }] * 10 + [{
-        '(Info) lolololol'
-    }] * 10
-i = 0
 while True:
-    i += 1
     try:
-        response = responses[i]
+        response = rak.receive()
         
         try:
             if response is not None:
                 strength = response['signal_strength']
                 noise = response['noise']
                 length = response['message_length']
-                print('------------------ Received message with signal strength {strength}, noise {noise} and length {length} ------------------')
                 message = response['message']
 
                 if not '(Info)' in message:
                     message_vals = message.split(';')
                     [cansat_id, timestamp, pressure, temperature, error_string, mode] = message_vals
-                    print(f'CanSat-ID: {cansat_id}, Timestamp: {timestamp}, Pressure: {pressure}hPa, Temperature: {temperature}°C, errors:{error_string}, {mode}')
-                    csv_writer.writerow([timestamp, pressure, temperature, error_string, mode, strength, noise, length])
+                    print(f'CanSat-ID: {cansat_id}, Timestamp: {timestamp}, Pressure: {pressure}hPa, Temperature: {temperature}°C, Errors: {error_string}, Mode: {mode}')
+
+                    with open(logfile_path, "a") as logfile:
+                        csv_writer = csv.writer(logfile, delimiter=";")
+                        csv_writer.writerow([timestamp, pressure, temperature, error_string, mode, strength, noise, length])
                 else:
                     print(message)
                     with open(infofile_path, "a") as logfile:
@@ -64,3 +60,4 @@ while True:
     
     except Exception as error:
         print('Error while receiving data: ' + str(error))
+        sleep(0.1)
